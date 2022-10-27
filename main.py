862d177b156c28477d2b88c0fde6216850183f80
Vethariel <dagraciap@unal.edu.co>
2022-10-26 21:42:56 -0500

    Main module left with console functions only

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import TablasCSV
-import re
-import string
+import TablasCSV, Usuario, Cuenta
 
 # funciones de logica
 
@@ -8,27 +6,6 @@ def no_loop_inf(a=[0]):
     a[0]+= 1
     if a[0]==6: a[0] = 1
     return a[0]<5
-
-def check_email_valido(email):
-    pat = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-    email_valido = re.match(pat,email)
-
-    return not email_valido
-
-def check_contra_valida(contra):
-    l, u, c, d = 0, 0, 0, 0
-    specialchar="$@_"
-    if (len(contra) >= 8):
-
-        for i in contra:
-            if (i in string.ascii_lowercase): l+=1           
-            if (i in string.ascii_uppercase): u+=1           
-            if (i in string.digits): d+=1           
-            if (i in specialchar): c+=1     
-
-    contra_valida = (l>=1 and u>=1 and c>=1 and d>=1 and l+c+u+d==len(contra))
-
-    return not contra_valida
 
 # funciones de interfaz
 
@@ -41,59 +18,73 @@ def crear_usuario(cuenta, usuarios):
     TablasCSV.crear_usuario_nuevo(usuarios,nombres,apellidos,nickname,fecha_nacimiento,cuenta)
     print("\nUsuario creado\n")
 
-def ingreso(cuentas):
+
+def ingreso(usuario, cuentas):
+    
     print("\n*Ingresar a su cuenta*\n")
     email = input("Escriba el email: ")
     contra = input("Escriba la contrasena: ")
-    ingreso, cuentaID = TablasCSV.check_ingreso(cuentas, email, contra)
+    ingreso = usuario.check_ingreso(cuentas, email, contra)
 
     while not ingreso:
+        
         print("ADV: Email o contrasena incorrecta")
         nueva_cuenta = input("Desea crear una cuenta? ").lower()
+        
         if nueva_cuenta == "si":
-            crear_cuenta(cuentas)
-            print("\n*Ingresar a su cuenta*\n")
-            email = input("Escriba el email: ")
-            contra = input("Escriba la contrasena: ")
-        ingreso, cuentaID = TablasCSV.check_ingreso(cuentas, email, contra)
+            crear_cuenta(usuario, cuentas)
+            
+        print("\n*Ingresar a su cuenta*\n")
+        email = input("Escriba el email: ")
+        contra = input("Escriba la contrasena: ")
+            
+        ingreso = usuario.check_ingreso(cuentas, email, contra)
 
     print(f"\nSatisfactoriamente logeado\n")
 
-    return cuentaID
+    return ingreso
 
-def crear_cuenta(cuentas):
+def crear_cuenta(usuario, cuentas):
 
     print("\n*Crear una nueva cuenta*\n")
     email = input("Escriba el email: ")
 
-    while check_email_valido(email):
+    while Cuenta.check_email_valido(email):
+        
         print("ADV: El email no es valido")
         email = input("Escriba el email: ")
 
-    while TablasCSV.check_email_existe(cuentas, email):
+    while Cuenta.check_email_existe(cuentas, email):
+        
         print("ADV: La cuenta ya existe")
         email = input("Escriba el email: ")
+        
+        while Cuenta.check_email_valido(email):
+            
+            print("ADV: El email no es valido")
+            email = input("Escriba el email: ")
 
     contra = input("Escriba la contrasena: ")
 
-    while check_contra_valida(contra):
+    while Cuenta.check_contra_valida(contra):
         print("ADV: La contrasena no es valida")
         contra = input("Escriba la contrasena: ")
 
     confirma_contra = input("Confirme la contrasena: ")
 
     while contra != confirma_contra:
+        
         print("ADV: Las contrasenas no coinciden")
         contra = input("Escriba la contrasena: ")
 
-        while check_contra_valida(contra):
+        while Cuenta.check_contra_valida(contra):
             print("ADV: La contrasena no es valida")
             contra = input("Escriba la contrasena: ")
 
         confirma_contra = input("Confirme la contrasena: ")
 
     # agrega la cuenta creada al archivo de cuentas
-    TablasCSV.crear_cuenta_nueva(cuentas, email, contra)
+    Cuenta.crear_cuenta_nueva(cuentas, email, contra)
     print("\nCuenta creada\n")
 
 #programa principal
@@ -101,12 +92,24 @@ def main():
 
     print("Bienvenido a MIR")
     
-    tablas_csv = TablasCSV()
-    cuentas = tablas_csv.cuentas_csv()
-    usuarios = tablas_csv.usuarios_csv()
-    patrones_usuario = tablas_csv.patrones_csv()
+    
+    nombre_csv = "cuentas.csv"
+    fieldnames = ["email", "contra", "accountID"]
+    cuentas = TablasCSV.TablasCSV(nombre_csv, fieldnames)
+    
+    nombre_csv = "usuarios.csv"
+    fieldnames = ["nombres", "apellidos", "nickname", "fecha_nacimiento",  "accountID"]
+    usuarios = TablasCSV.TablasCSV(nombre_csv, fieldnames)
+    
+    nombre_csv = "patrones.csv"
+    fieldnames = ["ruta_patron", "nombre_patron",  "accountID", "patronID"]     
+    patrones = TablasCSV.TablasCSV(nombre_csv, fieldnames)
+    
+    nombre_csv = "categorias.csv"
+    fieldnames = ["patronID", "categoria"]
+    categorias = TablasCSV.TablasCSV(nombre_csv, fieldnames)
 
-    cuenta = ""
+    usuario = Usuario.Usuario()
     logged = False
 
     interfaz1 = {"login":"Ingresar a su cuenta", "ncuenta":"Crear una nueva cuenta", "exit":"Salir"}
@@ -133,13 +136,12 @@ def main():
 
         if not logged:
             if comando == "login":
-                cuenta = ingreso(cuentas)
-                if not tablas_csv.check_usuario(cuenta, usuarios):
+                logged = ingreso(usuario, cuentas)
+                if not check_usuario(cuenta, usuarios):
                     crear_usuario(cuenta, usuarios)
-                logged = True
                 comandos = interfaz2
             if comando == "ncuenta":
-                crear_cuenta(cuentas)
+                crear_cuenta(usuario, cuentas)
         
         if logged:
             if comando == "signout":
