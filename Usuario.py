f2f3fdc68e8626594fa29c66f1e20701d8bf32eb
Vethariel <dagraciap@unal.edu.co>
2022-10-28 23:34:29 -0500

    Added docstring and delete user function

--- Usuario.py
+++ Usuario.py
@@ -1,7 +1,9 @@
 import Cuenta
+from TablasCSV import TablasCSV
 
 class Usuario(Cuenta.Cuenta):
-    
+    """Crea un objeto usuario y hereda dela clase Cuenta
+    """    
     def __init__(self) -> None:
         
         self.nombres = ""
@@ -10,8 +12,10 @@ class Usuario(Cuenta.Cuenta):
         self.fecha_de_nacimiento = ""
     
     
-    def check_info(self, usuarios):
-        
+    def check_info(self, usuarios:TablasCSV)->bool:
+        """Verifica que el usuario este registrado en el archivo csv de usuarios.
+        Si lo esta, asigna los datos a este objeto
+        """        
         datos_usuario = usuarios.check_dato({"accountID":self.accountID})
         
         if datos_usuario:
@@ -25,10 +29,36 @@ class Usuario(Cuenta.Cuenta):
         return datos_usuario
             
     
-    def crear_info(self, usuarios, nombres,apellidos,nickname,fecha_nacimiento):
+    def crear_info(self, usuarios:TablasCSV, nombres:str,
+                   apellidos:str,nickname:str,fecha_nacimiento:str):
+        """Agrega un nuevo registro en el archivo csv de usuarios
+        y asigna los datos a este objeto
+        """        
         
         usuarios.crear_registro_nuevo(nombres,apellidos,nickname,fecha_nacimiento,self.accountID)
         self.nombres = nombres
         self.apellidos = apellidos
         self.nickname = nickname
         self.fecha_de_nacimiento = fecha_nacimiento
+    
+    def eliminar_usuario(self,usuarios:TablasCSV):
+        """Elimina al usuario del registro del archivo csv de usuarios
+        """        
+        
+        usuarios.eliminar_registro({"accountID":self.accountID})
+        self.nombres = ""
+        self.apellidos = ""
+        self.nickname = ""
+        self.fecha_de_nacimiento = ""
+    
+    def vaciar_usuario(self):
+        """Desocupa este objeto para ser usado en otras instancias
+        """        
+        
+        self.email = ""
+        self.contra = ""
+        self.accountID = ""
+        self.nombres = ""
+        self.apellidos = ""
+        self.nickname = ""
+        self.fecha_de_nacimiento = ""
