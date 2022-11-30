e75e8c6d90c8bfda86d9ca21993827ef818fb15c
Vethariel <dagraciap@unal.edu.co>
2022-11-29 22:28:24 -0500

    Create user data added and return image

--- UI.py
+++ UI.py
@@ -28,6 +28,7 @@ class Ingreso_UI(QDialog):
         self.boton_ingreso.clicked.connect(self.ingreso)
         self.contra_in.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
         self.boton_crear_cuenta.clicked.connect(self.ir_crear_cuenta)
+        self.boton_regreso.clicked.connect(self.ir_bienvenida)
 
     def ingreso(self):
         email = self.email_in.text()
@@ -40,6 +41,10 @@ class Ingreso_UI(QDialog):
         elif not usuario.check_ingreso(cuentas, email, contra):
             adv = "Email o contrasena incorrecta"
         else:
+            if not usuario.check_info(usuarios):
+                crear_datos = Crear_Datos_UI()
+                widget.addWidget(crear_datos)
+                widget.setCurrentIndex(widget.currentIndex()+1)
             print("Ingreso")
         self.adv_ingreso.setText(adv)
     
@@ -47,7 +52,12 @@ class Ingreso_UI(QDialog):
         crear_cuenta = Crear_Cuenta_UI()
         widget.addWidget(crear_cuenta)
         widget.setCurrentIndex(widget.currentIndex()+1)
-
+    
+    def ir_bienvenida(self):
+        bienvenida = Bienvenida_UI()
+        widget.addWidget(bienvenida)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+        
 class Crear_Cuenta_UI(QDialog):
     def __init__(self):
         super(Crear_Cuenta_UI,self).__init__()
@@ -55,6 +65,7 @@ class Crear_Cuenta_UI(QDialog):
         self.boton_nueva_cuenta.clicked.connect(self.crear_cuenta)
         self.contra_in.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
         self.conf_contra_in.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
+        self.boton_regreso.clicked.connect(self.ir_bienvenida)
     
     def crear_cuenta(self):
         email = self.email_in.text()
@@ -79,6 +90,37 @@ class Crear_Cuenta_UI(QDialog):
             widget.setCurrentIndex(widget.currentIndex()+1)
         
         self.adv_ingreso.setText(adv)
+    
+    def ir_bienvenida(self):
+        bienvenida = Bienvenida_UI()
+        widget.addWidget(bienvenida)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+class Crear_Datos_UI(QDialog):
+    def __init__(self):
+        super(Crear_Datos_UI,self).__init__()
+        loadUi("crearDatosUsuario.ui",self)
+        self.boton_nuevo_usuario.clicked.connect(self.crear_datos)
+        self.boton_regreso.clicked.connect(self.ir_ingreso)
+    
+    def crear_datos(self):
+        nombres = self.nombres_in.text()
+        apellidos = self.apellidos_in.text()
+        nickname = self.nickname_in.text()
+        fecha_nacimiento = self.fecha_nacimiento_in.date()
+        adv = ""
+        if len(nombres)==0 or len(apellidos)==0 or len(nickname)==0:
+            adv = "Debe llenar todos los campos"
+        else:
+            usuario.crear_info(usuarios,nombres,apellidos,nickname,fecha_nacimiento)
+            print("Datos creados")
+        self.adv_ingreso.setText(adv)
+    
+    def ir_ingreso(self):
+        usuario.vaciar_usuario()
+        ingreso = Ingreso_UI()
+        widget.addWidget(ingreso)
+        widget.setCurrentIndex(widget.currentIndex()+1)
 
 
 nombre_csv = "cuentas.csv"
@@ -89,16 +131,7 @@ nombre_csv = "usuarios.csv"
 fieldnames = ["nombres", "apellidos", "nickname", "fecha_nacimiento",  "accountID"]
 usuarios = TablasCSV.TablasCSV(nombre_csv, fieldnames)
 
-nombre_csv = "patrones.csv"
-fieldnames = ["ruta_patron", "nombre_patron",  "accountID", "patronID"]     
-patrones = TablasCSV.TablasCSV(nombre_csv, fieldnames)
-
-nombre_csv = "categorias.csv"
-fieldnames = ["patronID", "categoria"]
-categorias = TablasCSV.TablasCSV(nombre_csv, fieldnames)
-
 usuario = Usuario.Usuario()
-logged = False
 
 app = QApplication(sys.argv)
 bienvenida = Bienvenida_UI()
